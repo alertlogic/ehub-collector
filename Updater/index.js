93a93bd2e140f1234efe99130197d4ff3fa9702b
imran.syed <imran.syed@alertlogic.com>
2022-09-26 13:24:50 +0530

    v3 point to im-update-v4-changes branch

--- Updater/index.js
+++ Updater/index.js
@@ -12,11 +12,9 @@
 
 const AlAzureUpdater = require('@alertlogic/al-azure-collector-js').AlAzureUpdater;
 
-const envObj = require('../al-ehub-collector.json');
-
 module.exports = function (context, AlertlogicUpdaterTimer) {
     var updater = new AlAzureUpdater();
-    updater.run(envObj.Runtime,function(syncError){
+    updater.run(function(syncError){
         if (syncError) {
             context.log.error('Site sync and env set failed:', syncError);
         } else {
